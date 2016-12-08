ffcbf496809e76eef35ba4dccedd904f2b2a28fa
nicugergely <nicugergely@gmail.com>
2016-12-08 16:39:03 +0200

    - tdc Instagram placeholder https://github.com/opradu/td-composer/issues/57

--- includes/shortcodes/td_block_instagram.php
+++ includes/shortcodes/td_block_instagram.php
@@ -2,6 +2,8 @@
 
 class td_block_instagram extends td_block {
 
+	private $atts = array();
+
 	/**
 	 * Disable loop block features. This block does not use a loop and it dosn't need to run a query.
 	 */
@@ -12,7 +14,12 @@ class td_block_instagram extends td_block {
 
     function render($atts, $content = null) {
 
-        parent::render($atts); // sets the live atts, $this->atts, $this->block_uid, $this->td_query (it runs the query)
+	    parent::render($atts); // sets the live atts, $this->atts, $this->block_uid, $this->td_query (it runs the query)
+
+	    $this->atts = shortcode_atts(
+            array(
+                'instagram_id' => '',
+            ), $atts);
 
 
         if (empty($td_column_number)) {
@@ -29,6 +36,11 @@ class td_block_instagram extends td_block {
             $buffy .= '<div class="td-block-title-wrap">';
                 $buffy .= $this->get_block_title(); //get the block title
             $buffy .= '</div>';
+
+		    // For tagDiv composer add a placeholder element
+	        if ((td_util::tdc_is_live_editor_iframe() || td_util::tdc_is_live_editor_ajax()) && '' === $this->atts['instagram_id']) {
+		        $buffy .= '<div class="td-block-missing-settings">Instagram block <strong>Instagram ID</strong> is empty. Configure this block and enter a valid Instagram id.</div>';
+	        }
 
             $buffy .= '<div id=' . $this->block_uid . ' class="td-instagram-wrap">';
                 $buffy.= td_instagram::render_generic($atts);
